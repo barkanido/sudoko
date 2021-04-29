f0e1dfcd5565e405a40067da9e953e3ed2d64d95
Ido Barkan <ido@appsflyer.com>
2021-04-29 09:18:16 +0300

    bench

--- src/lib.rs
+++ src/lib.rs
@@ -135,24 +135,6 @@ fn cube_vals(board: &[Option<u32>], row: usize, col: usize) -> HashSet<u32> {
     res
 }
 
-fn main() {
-    let expert_board = vec![
-        0, 0, 5, 0, 0, 0, 0, 6, 2, 0, 6, 3, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0,
-        0, 0, 6, 7, 0, 3, 0, 0, 6, 7, 0, 5, 0, 0, 0, 1, 0, 0, 8, 0, 0, 0, 0, 0, 8, 0, 1, 2, 0, 0,
-        6, 0, 0, 0, 0, 0, 0, 0, 0, 5, 3, 0, 0, 4, 0, 0, 0, 0, 8, 0, 0,
-    ];
-    let easy_board = vec![
-        0, 0, 0, 5, 9, 0, 0, 3, 7, 0, 7, 9, 0, 3, 2, 0, 8, 0, 0, 0, 8, 7, 0, 0, 0, 0, 0, 3, 0, 0,
-        0, 0, 1, 6, 2, 0, 0, 9, 0, 0, 0, 6, 3, 7, 0, 7, 2, 0, 3, 0, 0, 1, 5, 8, 0, 0, 0, 0, 0, 7,
-        8, 6, 5, 0, 8, 7, 0, 1, 0, 2, 4, 0, 0, 5, 4, 6, 0, 0, 7, 0, 0,
-    ];
-    // let mut game = Sudoku::from(expert_board);
-    let mut game = Sudoku::from(expert_board);
-    println!("{}", game);
-    solve_sudoku(&mut game);
-    println!("{}", game);
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -206,4 +188,23 @@ mod tests {
         let expected = vec![9, 8, 7].into_iter().collect::<HashSet<u32>>();
         assert_eq!(expected, options_for(&board, 4, 4)); // a spot in the middle cubem 4th row 4th col
     }
+
+    // fn bench_solve() {
+
+    // let expert_board = vec![
+    //     0, 0, 5, 0, 0, 0, 0, 6, 2, 0, 6, 3, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0,
+    //     0, 0, 6, 7, 0, 3, 0, 0, 6, 7, 0, 5, 0, 0, 0, 1, 0, 0, 8, 0, 0, 0, 0, 0, 8, 0, 1, 2, 0, 0,
+    //     6, 0, 0, 0, 0, 0, 0, 0, 0, 5, 3, 0, 0, 4, 0, 0, 0, 0, 8, 0, 0,
+    // ];
+    // let easy_board = vec![
+    //     0, 0, 0, 5, 9, 0, 0, 3, 7, 0, 7, 9, 0, 3, 2, 0, 8, 0, 0, 0, 8, 7, 0, 0, 0, 0, 0, 3, 0, 0,
+    //     0, 0, 1, 6, 2, 0, 0, 9, 0, 0, 0, 6, 3, 7, 0, 7, 2, 0, 3, 0, 0, 1, 5, 8, 0, 0, 0, 0, 0, 7,
+    //     8, 6, 5, 0, 8, 7, 0, 1, 0, 2, 4, 0, 0, 5, 4, 6, 0, 0, 7, 0, 0,
+    // ];
+    // // let mut game = Sudoku::from(expert_board);
+    // let mut game = Sudoku::from(expert_board);
+    // println!("{}", game);
+    // solve_sudoku(&mut game);
+    // println!("{}", game);
+    // }
 }
